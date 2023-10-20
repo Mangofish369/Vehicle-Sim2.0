ca92946815285120877bbcdf8c0449193706aef0
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2023-10-20 10:00:14 -0400

    Visual Clarity for AOE healing
    - Fully implemented a visual representation of Ambulance aoe heal area

--- Ambulance.java
+++ Ambulance.java
@@ -1,11 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
 
 /**
  * The Ambulance subclass
  */
 public class Ambulance extends Vehicle
 {
-    
+    public static final int HEAL_IMAGE_DURATION = 200;
+    SimpleTimer timer = new SimpleTimer();
+    HealEffect healEffect;
     public Ambulance(VehicleSpawner origin){
         super (origin); // call the superclass' constructor first
         
@@ -17,6 +20,8 @@ public class Ambulance extends Vehicle
         //System.out.println("Width: "+getImage().getWidth()+"\nHeight: "+getImage().getHeight());
         GreenfootImage ambulance = getImage();
         ambulance.scale(100,50);
+        
+        healEffect = new HealEffect();
     }
 
     /**
@@ -25,14 +30,29 @@ public class Ambulance extends Vehicle
      */
     public void act()
     {
+        if(timer.millisElapsed() >= HEAL_IMAGE_DURATION){
+            getWorld().removeObject(healEffect);    
+            //getWorld().removeObjects(getWorld().getObjects(HealEffect.class));
+        }
         super.act();
     }
 
     public boolean checkHitPedestrian () {
+        /*
         Pedestrian p = (Pedestrian)getOneObjectAtOffset((int)speed + getImage().getWidth()/2, 0, Pedestrian.class);
         if(p != null && !p.isAwake()){
             p.healMe();
             return true;
+        }
+        */
+        
+        ArrayList<Pedestrian> peds = (ArrayList<Pedestrian>)getObjectsInRange(100,Pedestrian.class);
+        for(Pedestrian pe : peds){
+            if(pe != null && !pe.isAwake()){
+                pe.healMe();
+                getWorld().addObject(healEffect,getX(),getY());
+                timer.mark();
+            }
         }
         return false;
     }
